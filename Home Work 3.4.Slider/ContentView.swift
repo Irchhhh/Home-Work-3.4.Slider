e3f15b36ad17115f336b6cf2b28464c195676c76
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-01-31 14:27:32 +0300

    Initial Commit

---
+++ Home Work 3.4.Slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Home Work 3.4.Slider
+//
+//  Created by Ирина on 31.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Home Work 3.4.Slider/Home_Work_3_4_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Work_3_4_SliderApp.swift
+//  Home Work 3.4.Slider
+//
+//  Created by Ирина on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Home_Work_3_4_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
