3ef5379cf127e1dd03819eada48844e4254eca44
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-02-01 12:37:06 +0300

    first commit

---
+++ Home Work 3.4.Slider/ButtonViews.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Home Work 3.4.Slider
+//
+//  Created by Ирина on 31.01.2022.
+//
+
+import SwiftUI
+
+struct ButtonViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonViews()
+    }
+}
---
+++ Home Work 3.4.Slider/SliderView.swift
@@ -0,0 +1,33 @@
+//
+//  UISlider.swift
+//  Home Work 3.4.Slider
+//
+//  Created by Ирина on 31.01.2022.
+//
+
+import SwiftUI
+
+struct SliderView: UIViewRepresentable  {
+    func makeUIView(context: Context) -> UISlider {
+        <#code#>
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        <#code#>
+    }
+    
+    
+    typealias UIViewType = UISlider
+    
+    
+    
+    
+    
+    }
+
+
+struct UISlider_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView()
+    }
+}
